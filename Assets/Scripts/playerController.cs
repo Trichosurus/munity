13eee28d6c0cfa52463f1b18738d2726ef8834fe
Trichosurus <sloshy@acinonyx.me>
2018-03-14 23:37:09 +1100

    Added player spawning

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour {
 
+	public float accelleration = 1;
+	public float maxspeed = 3;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+		// Update is called once per frame
+
 	void Update () {
+
+		
 		
 	}
+
+
+	/// <summary>
+	/// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void FixedUpdate()
+	{
+		Rigidbody rb = GetComponent<Rigidbody>();
+
+
+		float z = 0;
+		float x = 0;
+		if (Input.GetKey("w")){z += 1;}
+		if (Input.GetKey("s")){z -= 1;}
+		if (Input.GetKey("a")){x -= 1;}
+		if (Input.GetKey("d")){x += 1;}
+
+		Vector3 fwspeed = transform.forward * accelleration * z;
+		Vector3 rtspeed = transform.right * accelleration * x;
+		if (rb.velocity.magnitude < maxspeed) {
+			rb.velocity += fwspeed + rtspeed;
+		}
+	}
+
 }
